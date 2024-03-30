85074b3fddd5a2d7e36a76313cfd0ffb2d0e0a25
Abul Ala Jobayar <jobayar59@gmail.com>
2024-03-30 17:42:21 +0600

    update server

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,34 @@ import config from "./app/config";
 
 
 let server:Server
-function main() {
- server= app.listen(config.port, () => {
-    console.log(` blood Bucket listening on port ${config.port} `);
-  });
+async function main() {
+  try {
+    server= app.listen(config.port, () => {
+      console.log(` blood Bucket listening on port ${config.port} `);
+    });
+  } catch (error) {
+    console.log(error)
+  }
+
 }
 
 main();
+process.on('unhandledRejection', () => {
+  console.log(`unhandled Rejection is Detected,shuting Down `)
+  if (server) {
+      server.close(() => {
+          process.exit(1);
+      })
+  }
+  process.exit(1)
+});
+process.on('uncaughtException', () => {
+  console.log(`uncaught Exception is detected , shuting Down`)
+  process.exit(1)
+});
+
+
+
+
+
+
